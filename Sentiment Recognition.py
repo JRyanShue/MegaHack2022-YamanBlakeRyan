0b65e8c59021caee6f5c5a387835feb9ccfca090
Yaman-Habip <yamanhabip@icloud.com>
2022-04-02 16:50:27 -0400

    Finished Image Mood Recog.

--- Sentiment Recognition.py
+++ Sentiment Recognition.py
@@ -1,6 +1,8 @@
 import cv2
 from deepface import DeepFace
 
-def face_sentiment(img):
-
-    return
+def face_sentiment(img): #expects string with image title
+    to_analyze = cv2.imread(img)
+    analysis = DeepFace.analyze(to_analyze)
+    mood = analysis["dominant_emotion"]
+    return mood
