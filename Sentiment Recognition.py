6cdab4c3ffe194cd75238191f5b5543040f9f1da
Yaman-Habip <yamanhabip@icloud.com>
2022-04-02 21:08:40 -0400

    Added review sentiment analyzer

--- Sentiment Recognition.py
+++ Sentiment Recognition.py
@@ -1,9 +1,13 @@
 def face_sentiment(img): #expects string with image title
+
     import cv2
     from deepface import DeepFace
+
     to_analyze = cv2.imread(img)
+
     analysis = DeepFace.analyze(to_analyze)
     mood = analysis["dominant_emotion"]
+
     if mood == "angry":
         return "😡"
     elif mood == "disgust":
@@ -19,5 +23,27 @@ def face_sentiment(img): #expects string with image title
     else:
         return "😐"
 
- def review_sentiment(review):
-     return
+def review_sentiment(review): #expects string with review
+
+    #Access Hugging Face BERT API
+
+    import requests
+
+    url = "https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment"
+    token = "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"
+    headers = {"Authorization": f"Bearer {token}"}
+
+    to_input = {"inputs": review}
+
+    response = requests.post(url, headers = headers, json = to_input)
+
+    output = response.json()
+
+    rating = 0
+
+    for i in range(5):
+        rating += output[0][i]["score"] * (i + 1)
+
+    rating *= 20 #turn rating into a score out of 100
+
+    return rating
