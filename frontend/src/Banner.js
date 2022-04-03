c00f2b97226ce8e1ba8ea4393432eb802cb3e7cb
JRyanShue <ryan_shue23@milton.edu>
2022-04-02 21:38:52 -0400

    update react

--- frontend/src/Banner.js
+++ frontend/src/Banner.js
@@ -1,8 +1,12 @@
 
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 
 var exampleJson = {
   "available_therapists": [
+    {"name": "Dr. Smith", "id": "1", "score": "90.5"},
+    {"name": "Dr. Jones", "id": "2", "score": "80.5"},
+    {"name": "Dr. Brown", "id": "3", "score": "70.5"},
+    {"name": "Dr. Wilson", "id": "4", "score": "60.5"},
     {"name": "Dr. Smith", "id": "1", "score": "90.5"},
     {"name": "Dr. Jones", "id": "2", "score": "80.5"},
     {"name": "Dr. Brown", "id": "3", "score": "70.5"},
@@ -16,20 +20,48 @@ function Banner() {
 
   const [therapistsDisplay, setTherapistsDisplay] = useState(<div></div>);
 
+  // console.log(availableTherapists["available_therapists"].map((therapist) => {
+
+  //   {
+  //     return (
+  //       <div>
+  //         {therapist}
+  //       </div>
+  //     )
+  //   }
+
+  // }))
+
   function updateTherapistsDisplay( availableTherapists ) {
 
-    for (therapist in availableTherapists["available_therapists"]) {
+    console.log('available therapists:', availableTherapists.available_therapists)
 
-      
-
-    }
+    setTherapistsDisplay( availableTherapists.available_therapists.map((therapist) => {
+      {
+        console.log('therapist:', therapist)
+        return (
+          <div>
+            {therapist.name} - {therapist.score}/100
+          </div>
+        )
+      }
+    }));
 
   }
 
+  useEffect(() => {
+    updateTherapistsDisplay( availableTherapists );
+  }, []);
+
   return (
     <div className="banner">
-      Hello World!
-      {JSON.stringify(availableTherapists)}
+      <div className="label">
+        Available People to Talk To:
+      </div>
+      <br/>
+      <div className="therapists-list">
+        {therapistsDisplay}
+      </div>
     </div>
   );
 }
